3b76aa9c9c64dd52d5d1e9e4f7394162677e2bbd
celesteg15 <166965352+celesteg15@users.noreply.github.com>
2025-04-24 22:37:48 +0000

    renamed files

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/common/TimerModelListener.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/common/TimerModelListener.java
@@ -7,7 +7,7 @@ package edu.luc.etl.cs313.android.simplestopwatch.common;
  *
  * @author laufer
  */
-public interface StopwatchModelListener {
+public interface TimerModelListener {
     void onTimeUpdate(int timeValue);
     void onStateUpdate(int stateId);
 }
