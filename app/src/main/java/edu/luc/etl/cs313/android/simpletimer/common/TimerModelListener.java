c1870c4f94373f248c147e3e3d99c7ca2db10ba0
celesteg15 <166965352+celesteg15@users.noreply.github.com>
2025-04-26 19:56:20 +0000

    common folder comments

--- app/src/main/java/edu/luc/etl/cs313/android/simpletimer/common/TimerModelListener.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simpletimer/common/TimerModelListener.java
@@ -8,6 +8,6 @@ package edu.luc.etl.cs313.android.simpletimer.common;
  * @author laufer
  */
 public interface TimerModelListener {
-    void onTimeUpdate(int timeValue);
-    void onStateUpdate(int stateId);
+    void onTimeUpdate(int timeValue); // called in order to update the runtime value on the UI
+    void onStateUpdate(int stateId); // updates the current state (stooped, running, beeping)
 }
--- app/src/main/java/edu/luc/etl/cs313/android/simpletimer/common/TimerModelSource.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simpletimer/common/TimerModelSource.java
@@ -7,5 +7,9 @@ package edu.luc.etl.cs313.android.simpletimer.common;
  * @author laufer
  */
 public interface TimerModelSource {
+    /**
+     * This registers a TimerModelListener to recevie updates from the model 
+     * @param listener the listener that will recevie time and state updates
+     */
     void setModelListener(TimerModelListener listener);
 }
