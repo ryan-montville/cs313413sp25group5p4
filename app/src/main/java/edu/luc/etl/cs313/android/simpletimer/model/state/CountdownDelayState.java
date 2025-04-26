e5c911cbd5b361e5700a0287192e540653e871dc
p2pcodest <dduvic@luc.edu>
2025-04-26 15:58:05 -0500

    countDownDeleyStateCom

--- app/src/main/java/edu/luc/etl/cs313/android/simpletimer/model/state/CountdownDelayState.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simpletimer/model/state/CountdownDelayState.java
@@ -2,6 +2,13 @@ package edu.luc.etl.cs313.android.simpletimer.model.state;
 
 import edu.luc.etl.cs313.android.simpletimer.R;
 
+/**
+ * The CountdownDelayState class represents the initial delay period
+ * before the timer starts running. This state is entered when the user
+ * initiates the start action, providing countdown.
+ * It implements the TimerState interface, defining the behavior
+ * of the timer during this phase.
+ */
 public class CountdownDelayState implements TimerState {
 
     public CountdownDelayState(final TimerSMStateView sm) {
