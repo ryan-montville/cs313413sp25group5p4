8c2ba657ef6163d884c5d0a5b1de153e7ecda8d7
p2pcodest <dduvic@luc.edu>
2025-04-26 15:23:26 -0500

    InitialBeepingDoc

--- app/src/main/java/edu/luc/etl/cs313/android/simpletimer/model/state/BeepingState.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simpletimer/model/state/BeepingState.java
@@ -2,8 +2,16 @@ package edu.luc.etl.cs313.android.simpletimer.model.state;
 
 import edu.luc.etl.cs313.android.simpletimer.R;
 
+        /** The BeepingState class represents the state of the timer when it is emitting a beep sound.
+        * This state is entered when the timer reaches zero.
+        * It implements the {@link TimerState} interface, defining the behavior of the timer in this specific state.
+        */
 public class BeepingState implements TimerState {
-
+    /**
+     * Constructor for BeepingState instance.
+     *
+     * @param sm The state machine view associated with this state.
+     */
     public BeepingState(final TimerSMStateView sm) {
         this.sm = sm;
     }
